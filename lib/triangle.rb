d5da6e665e840547bef0954b95a9f2d17051750f
Joshua Clarence Sadsad <joshsad2@gmail.com>
2018-09-25 00:08:06 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,9 +8,10 @@ class Triangle
  end
  
  def kind
-  if arg1 == b && b == c
+   
+  if arg1 == arg2 && arg2 == arg3
     :equilateral
-  elsif arg1 == b || b == c || arg1 == c
+  elsif arg1 == arg2 || arg2 == arg3 || arg1 == arg3
     :isosceles
   else
     :scalene
@@ -19,9 +20,9 @@ class Triangle
   class TriangleError < StandardError
   end
   
-  def validate_triangle
-    real_triangle = [(arg1 + b > c), (arg1 + c > b), (b + c > arg1)]
-    [a, b, c].each { |s| real_triangle << false if s <= 0 }
+  def true_triangle
+    real_triangle = [(arg1 + arg2 > arg3), (arg1 + arg3 > arg2), (arg2 + arg3 > arg1)]
+    [arg1, arg2, arg3].each { |s| real_triangle << false if s <= 0 }
     raise TriangleError if real_triangle.include?(false)
   end
 end
